c3324d5acb2863415da27e09a247a0059412ff94
jakemraz <jakemraz100@gmail.com>
2021-08-24 21:03:29 +0900

    add more method

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log('GET /');
+  res.status(200).json({
+    message: 'GET /',
+  });
+});
+
+app.post('/', (req, res) => {
+  console.log('POST /');
+  res.status(200).json({
+    message: 'POST /',
+  });
+});
+
+app.all('/hello', (req, res) => {
+  console.log(`${req.method} /hello`);
+  res.status(200).json({
+    message: `${req.method} /hello`,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
 
+exports.app = app;
